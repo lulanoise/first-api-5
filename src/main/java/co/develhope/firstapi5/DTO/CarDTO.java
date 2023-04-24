10918a1a16963e64817724da35ad2bae3e0cf4ea
lulanoise <lucrezia.rumore98@gmail.com>
2023-04-24 18:03:11 +0200

    removed lombok

--- src/main/java/co/develhope/firstapi5/DTO/CarDTO.java
+++ src/main/java/co/develhope/firstapi5/DTO/CarDTO.java
@@ -1,19 +1,54 @@
 package co.develhope.firstapi5.DTO;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class CarDTO {
     @NotBlank(message="Mandatory!")
     private String id;
     @NotBlank (message="Mandatory!")
     private String modelName;
     private double price;
+
+    public CarDTO(String id, String modelName, double price) {
+        this.id = id;
+        this.modelName = modelName;
+        this.price = price;
+    }
+    public CarDTO(){}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "CarDTO{" +
+                "id='" + id + '\'' +
+                ", modelName='" + modelName + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
 }
